d1c3e1b22e7311edf6210b96984f632116987f60
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-10-11 10:35:44 -0300

    att

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Section from '@/components/Section'
+import React from 'react'
+
+const About = () => {
+  return (
+    <Section>
+      <div>About</div>
+    </Section>
+  )
+}
+
+export default About
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Section from "@/components/Section";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-
-    </main>
+    <Section className="flex gap-4">
+      <Link href="/">home</Link>
+      <Link href="/store">store</Link>
+      <Link href="/about">about</Link>
+    </Section>
   )
 }
