745e17e77c5fdf0caba57c2ebb4ef8378cd62397
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-11-01 15:27:40 -0300

    Att

--- src/components/layout/StoreItem.tsx
+++ src/components/layout/StoreItem.tsx
@@ -27,13 +27,13 @@ const StoreItem = ({id, name, price, imgUrl }: StoreItemProps) => {
 
 			<div className='flex flex-col gap-4 items-center px-4 pb-4'>
 				{quantity === 0 ? (
-					<button className='bg-blue-500 p-2 rounded-md' onClick={() => increaseCardQuantity(id)}>Add to card</button>
+					<button className='bg-lime-500 p-2 rounded-md' onClick={() => increaseCardQuantity(id)}>Add to card</button>
 				) : 
 				<div className='flex flex-col gap-4 items-center'>
 					 <div className='flex items-center gap-4'>
-						<button className='text-3xl bg-blue-500 px-2 rounded-md' onClick={() => decreaseCardQuantity(id)}>-</button>
+						<button className='text-3xl bg-lime-500 px-2 rounded-md' onClick={() => decreaseCardQuantity(id)}>-</button>
 						<span>{quantity} in cart</span>
-						<button className='text-3xl bg-blue-500 px-2 rounded-md' onClick={() => increaseCardQuantity(id)}>+</button>
+						<button className='text-3xl bg-lime-500 px-2 rounded-md' onClick={() => increaseCardQuantity(id)}>+</button>
 					 </div>
 					 <div>
 					 	<button className='bg-red-500 p-2 rounded-md' onClick={() => removeFromCart(id)}>Remove</button>
