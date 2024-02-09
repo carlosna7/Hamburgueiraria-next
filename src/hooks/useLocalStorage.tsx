4bb27a44d47b80f963d8a3d235b71745a53d2a19
Carlos Eduardo <carloseduardo.na7@gmail.com>
2024-02-09 10:54:32 -0300

    att - localStorage

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -3,10 +3,13 @@
 import { useEffect, useState } from "react"
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
-  
+  const isClient = typeof window === 'object' // Verifica se estamos no lado do cliente
+
   const [value, setValue] = useState<T>(() => {
-    const jsonValue = localStorage.getItem(key)
-    if (jsonValue != null) return JSON.parse(jsonValue)
+    if (isClient) {
+      const jsonValue = localStorage.getItem(key)
+      if (jsonValue != null) return JSON.parse(jsonValue)
+    }
 
     if (typeof initialValue === "function") {
       return (initialValue as () => T)()
@@ -16,8 +19,10 @@ export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   })
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value))
-  }, [key, value])
+    if (isClient) {
+      localStorage.setItem(key, JSON.stringify(value))
+    }
+  }, [isClient, key, value])
 
   return [value, setValue] as [typeof value, typeof setValue]
 }
