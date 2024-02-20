c5e7a5ea4d87763e4779c53b31ea6ab87f2b9e5b
Carlos Eduardo <carloseduardo.na7@gmail.com>
2024-02-20 10:39:30 -0300

    att

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -8,7 +8,7 @@ const Apps = () => {
   return (
   <>
   
-    <div className='relative h-full bg-amber-900 flex px-48 p-8'>
+    <section className='relative h-full bg-amber-900 flex px-48 p-8'>
       <div className='w-1/2 flex flex-col gap-8'>
         <h1 className='text-8xl text-amber-50'>Prático, rápido e cheio de benefícios</h1>
         <h2 className='text-6xl text-amber-200'>Venha para o nosso APP!</h2>
@@ -29,11 +29,11 @@ const Apps = () => {
       </div>
 
       <div className='w-1/2'>
-
+        
       </div>
-    </div>
+    </section>
 
-    <div className='flex flex-col justify-between px-48 p-8 gap-8'>
+    <section className='flex flex-col justify-between px-48 p-8 gap-8'>
       <h3 className='text-4xl '>Vantagens exclusivas do APP!</h3>
       
       <div className='flex items-center justify-between gap-8 relative' >
@@ -59,14 +59,49 @@ const Apps = () => {
           <p className='z-10'>Todos os benefícios e vantagens do Clube na palma da sua mão.</p>
         </div>
       </div>
-    </div>
+    </section>
 
+    <section className='bg-amber-100 flex flex-col px-48'>
 
+      <div className='flex'>
+        <img src="/app/phone-removebg-preview.png" alt="" />
 
-    <div className='bg-amber-100 flex justify-between px-48 p-8'>
-      <h3 className='text-4xl text-amber-900'>Faça um PIX ou pague como quiser</h3>
+        <div className='flex flex-col justify-center gap-8'>
+          <h3 className='text-4xl text-amber-900'>Faça um PIX ou pague como quiser</h3>
+          <p>Na nossam Hamburgueria, as formas de pagamento são práticas e seguras, seja pelo PIX, carteiras digitais ou seu cartão preferido.</p>
 
-    </div>
+          <div className='bg-white flex justify-between p-4 rounded-xl'>
+
+            <div>
+              <p>Carteira digitais</p>
+              <div className='flex pt-2'>
+                <img className='h-6' src="/app/pix.png" alt="" />
+                <img className='h-6' src="/app/mercadopago.png" alt="" />
+                <img className='h-6' src="/app/ame.png" alt="" />
+              </div>
+            </div>
+
+            <div>
+              <p>Crédito e débito</p>
+              <div className='flex pt-2'>
+                <img className='h-6' src="/app/mastercard.png" alt="" />
+                <img className='h-6' src="/app/elo.png" alt="" />
+                <img className='h-6' src="/app/visa.png" alt="" />
+              </div>
+            </div>
+
+            <div>
+              <p>Vale refeição</p>
+              <div className='flex pt-2'>
+                <img className='h-6' src="/app/cielo.png" alt="" />
+                <img className='h-6' src="/app/ticket.png" alt="" />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
 
   </>
   )
