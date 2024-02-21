cf10fdff733c8deb44977a1905a844f73183e9d2
Carlos Eduardo <carloseduardo.na7@gmail.com>
2024-02-21 17:19:22 -0300

    att

--- src/components/elements/FilterModal.tsx
+++ src/components/elements/FilterModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { FaXmark, FaBurger, FaBacon } from 'react-icons/fa6'
+import { FaXmark, FaBurger, FaBacon, FaCheck  } from 'react-icons/fa6'
 import { BsCupStraw } from 'react-icons/bs'
-import { GiChickenLeg, GiNachos } from 'react-icons/gi'
+import { GiChickenLeg, GiConsoleController, GiNachos } from 'react-icons/gi'
 
 interface TModal {
   isOpen: boolean
@@ -19,6 +19,7 @@ interface TModal {
 }
 
 const FilterModal: React.FC<TModal> = ({ isOpen, setIsOpen, setCarneAberto, setFrangoAberto, setBaconAberto, setAcompAberto, setBebidaAberto }) => {
+  
   const closeModal = () => {
     console.log('fechado')
     setIsOpen((prev) => !prev)
@@ -26,47 +27,68 @@ const FilterModal: React.FC<TModal> = ({ isOpen, setIsOpen, setCarneAberto, setF
 
   const toggleState = (setState: React.Dispatch<React.SetStateAction<boolean>>) => {
     setState((prev) => !prev)
+
+    console.log(toggleState)
   }
 
   return isOpen ? (
     <div className='fixed rounded inset-1/2 translate-x-[-50%] translate-y-[-50%] h-full w-full'>
       <div className='bg-black opacity-40 h-full w-full absolute'></div>
 
-      <div className='absolute bg-white w-[700px] h-[450px] inset-1/2 translate-x-[-50%] translate-y-[-50%] text-amber-900 rounded-xl'>
+      <div className='absolute bg-white w-[700px] h-[450px] inset-1/2 translate-x-[-50%] translate-y-[-50%] text-amber-900 rounded-xl p-8'>
         <div>
-          <h3 className='flex text-2xl justify-center pt-4'>Categorias</h3>
+          <h3 className='flex justify-center text-4xl pb-8'>Categorias</h3>
           <button
             onClick={closeModal}
-            className='absolute text-3xl right-4 top-4'
+            className='absolute text-3xl right-8 top-8'
           >
             <FaXmark />
           </button>
         </div>
 
-        <div>
-          <div onClick={() => toggleState(setCarneAberto)}>
+        <div className='flex flex-col gap-4'>
+          <div className='flex items-center gap-2 text-xl'>
+            <div className='border-2 border-black w-6 h-6' onClick={() => toggleState(setCarneAberto)}>
+              <FaCheck className='text-black' />
+            </div>
             <p>Sanduíche de Carne</p>
-            <FaBurger />
+            <FaBurger  />
           </div>
-          <div onClick={() => toggleState(setFrangoAberto)}>
+
+          <div className='flex items-center gap-2 text-xl'>
+            <div className='border-2 border-black w-6 h-6' onClick={() => toggleState(setFrangoAberto)}>
+              <FaCheck className='text-black' />
+            </div>
             <p>Sanduíche de Frango</p>
             <GiChickenLeg />
           </div>
-          <div onClick={() => toggleState(setBaconAberto)}>
+
+          <div className='flex items-center gap-2 text-xl'>
+            <div className='border-2 border-black w-6 h-6' onClick={() => toggleState(setBaconAberto)}>
+              <FaCheck className='text-black' />
+            </div>
             <p>Sanduíche com Bacon</p>
             <FaBacon />
           </div>
-          <div onClick={() => toggleState(setAcompAberto)}>
+
+          <div className='flex items-center gap-2 text-xl'>
+            <div className='border-2 border-black w-6 h-6' onClick={() => toggleState(setAcompAberto)}>
+              <FaCheck className='text-black' />
+            </div>
             <p>Acompanhamentos</p>
             <GiNachos />
           </div>
-          <div onClick={() => toggleState(setBebidaAberto)}>
+
+          <div className='flex items-center gap-2 text-xl'>
+            <div className='border-2 border-black w-6 h-6' onClick={() => toggleState(setBebidaAberto)}>
+              <FaCheck className='text-black' />
+            </div>
             <p>Bebidas</p>
             <BsCupStraw />
           </div>
         </div>
 
-        <div className='flex items-end'>
+        <div className='flex items-end pt-8'>
           <button
             onClick={closeModal}
             className=' bg-red-500 text-white py-2 px-24 rounded text-xl'
